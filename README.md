a513fcee5e5eca6b61627070df4bba6864ed5224
Yoshi Sakai <yoshi.sakai@gmail.com>
2018-08-21 17:48:20 +0900

    insert teble as like controller

--- README.md
+++ README.md
@@ -32,3 +32,24 @@ select * from sqlite_master;
 
 rake db:seed
 
+## データ初期化手順
+
+```
+以下２つのDBファイルを削除
+db/development.sqlite3
+db/test.sqlite3
+# DB作成、テーブル更新、初期データ投入
+rails db:create
+rails db:migrate
+rake db:seed
+```
+
+# 動作確認
+
+## ランキング表示
+
+http://localhost:3000/api/ranking/
+
+## 投票
+
+http://localhost:3000/api/like/?candidate_id=1&session_id=xyz
--- poll/app/controllers/api/like_controller.rb
+++ poll/app/controllers/api/like_controller.rb
@@ -1,8 +1,13 @@
 module Api
-  class Api::RankingController < ApplicationController
+  class Api::LikeController < ApplicationController
     def index
-      @ranking = Polling.find(Like.group(:candidate_id).order('count(candidate_id) desc').limit(9).pluck(:candidate_id))
-      render json: @ranking
+      poll = params.permit(:candidate_id, :session_id)
+      @polling = Polling.new(poll)
+      if @polling.save
+        render json: @polling
+      else
+        render json: @polling
+      end
     end
   end
 end
