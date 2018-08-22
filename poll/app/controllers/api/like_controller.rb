55f7d5af6efc871e35afd9952f575efbcd7c0672
Yoshi Sakai <yoshi.sakai@gmail.com>
2018-08-22 12:45:47 +0900

    update polling data any time and sdd session_id

--- poll/app/controllers/api/like_controller.rb
+++ poll/app/controllers/api/like_controller.rb
@@ -1,11 +1,29 @@
+require 'digest/sha2'
+
 module Api
+
   class Api::LikeController < ApplicationController
     def index
-      poll = params.permit(:candidate_id, :session_id)
-      @polling = Polling.new(poll)
-      if @polling.save
-        render json: @polling
+
+      t = (cookies['count'] || 1).to_i
+      if t == 1
+        ## session id を時刻をハッシュ化して設定
+        cookies['SSID'] = Digest::SHA256.hexdigest Time.now.strftime("%c")
+      end
+      params['session_id'] = cookies['SSID']
+
+      if t > 1
+        @polling = Polling.find_by(session_id: cookies['SSID'])
+        @polling.candidate_id = params['candidate_id']
       else
+        poll = params.permit(:candidate_id, :session_id)
+        @polling = Polling.new(poll)
+      end
+
+      cookies['count'] = t + 1
+
+      ## DBに保存実行
+      if @polling.save
         render json: @polling
       end
     end
